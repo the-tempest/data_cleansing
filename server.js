6f0cea8e69fa5794651488b0c145a5931a5dc545
Nurik <ntussipov@gmail.com>
2016-06-29 23:01:29 -0500

    added node server and a simple test html for uploading files

---
+++ server.js
@@ -0,0 +1,97 @@
+/**
+ * This file provided by Facebook is for non-commercial testing and evaluation
+ * purposes only. Facebook reserves all rights not expressly granted.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * FACEBOOK BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+var fs = require('fs-extra');
+var path = require('path');
+var express = require('express');
+var bodyParser = require('body-parser');
+var request = require('request');
+var app = express();
+var util = require('util');
+var formidable = require('formidable');
+var http = require('http');
+var PythonShell = require('python-shell');
+
+
+
+
+app.set('port', (process.env.PORT || 3000));
+
+app.use('/', express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+// Additional middleware which will set headers that we need on each request.
+app.use(function(req, res, next) {
+    // Set permissive CORS header - this allows this server to be used only as
+    // an API server in conjunction with something like webpack-dev-server.
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Disable caching so we'll always get the latest comments.
+    res.setHeader('Cache-Control', 'no-cache');
+    next();
+});
+
+app.post('/process', function(req, resp) {
+
+    var form = new formidable.IncomingForm();
+    form.parse(req, function (err, fields, files) {
+        if (err) {
+            console.error(err.message);
+            return;
+        }
+        var data = "Hello from the Node writeFile method!";
+
+        console.log(files['file']);
+
+        resp.writeHead(200, {'content-type': 'text/plain'});
+        resp.write('received upload:\n\n');
+        resp.end(util.inspect({fields: fields, files: files}));
+    });
+    /*var pyshell = new PythonShell('my_script.py');
+    pyshell.send('hello');
+
+    pyshell.on('message', function (message) {
+        // received a message sent from the Python script (a simple "print" statement)
+        console.log(message);
+    });
+
+    pyshell.end(function (err) {
+        if (err) throw err;
+        console.log('finished');
+    });*/
+    form.on('end', function(fields, files) {
+       /* Temporary location of our uploaded file */
+       var temp_path = this.openedFiles[0].path;
+       /* The file name of the uploaded file */
+       var file_name = this.openedFiles[0].name;
+       /* Location where we want to copy the uploaded file */
+       var new_location = './uploaded/';
+
+       fs.copy(temp_path, new_location + file_name, function(err) {
+           if (err) {
+               console.error(err);
+           } else {
+               console.log("success!")
+           }
+       });
+     });
+
+    return;
+});
+
+
+
+
+app.listen(app.get('port'), function() {
+  console.log('Server started: http://localhost:' + app.get('port') + '/');
+});
