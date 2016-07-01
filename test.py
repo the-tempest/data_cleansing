66e8c5b1c5fab96694c947bf2455586940d12642
Nurik <ntussipov@gmail.com>
2016-07-01 11:21:39 -0500

    looping through columns, calling column_typer

--- test.py
+++ test.py
@@ -1,9 +1,11 @@
 import os, sys, csv, mysql.connector
 
+#execfile('column_type.py');
 # db connector stuff
 cnx = mysql.connector.connect(user='root', password='123', host='localhost', database='world')
 cursor = cnx.cursor();
 # sys.argv[1]
+#"./uploaded/SalesJan2009.csv"
 fn, file_extension = os.path.splitext(sys.argv[1]);
 filename = os.path.basename(fn);
 if (file_extension == '.csv'):
@@ -28,15 +30,23 @@ if (file_extension == '.csv'):
         for i in range(len(columns)):
             s.append('%s');
         query = query.format(','.join(columns), ','.join(s))
-        print query;
+        #print query;
 
         for data in reader:
-            print query
-            print data
+            #print query
+            #print data
             cursor.execute(query, data)
 
 #elif (file_extension == '.json'):
 
+for i in range (len(columns)):
+    query = "SELECT " + columns[i] + " FROM " + filename;
+    cursor.execute(query);
+    rows = cursor.fetchall();
+    for i in range(len(rows)):
+        rows[i] = str(''.join(rows[i]));
+    #coltyper = column_typer(rows);
+    #coltyper.column_parser();
 cnx.commit()
 cursor.close()
 cnx.close()
