74a7f2e68dd09a141509a0ef4171c22d7e900a06
WilliamWallace1272 <williamwallace2018@u.northwestern.edu>
2016-07-06 14:40:42 -0500

    worked on bayes classification for a list of features and columns types. Not yet tested

--- numeric_classifier.py
+++ numeric_classifier.py
@@ -1,4 +1,4 @@
-import os, math
+import os, math, operator
 
 class numeric_classifier:
 	def __init__(self):
@@ -12,19 +12,40 @@ class numeric_classifier:
 		for type in self.numeric_types: # going to check that it can be of any type
 			type_probabilities[type.name] = 1
 			for feature, feature_occurance in type.feature_dictionary: #for a specific type compute P(feature | type)
+				
 				curr_dictionary = feature_occurance
 				total_number = sum(curr_dictionary.itervalues())
-				self.type_switch
+				posterier_type_prob = self.type_switch(feature, nText, curr_dictionary)
+				type_probabilities[type.name] *= posterier_type_prob
 
+		#find the max of all types to guess what the type of the column is
+		max(type_probabilities.iteritems(), key=operator.itemgetter(1))[0] 
+		
+	
 
-	def type_switch(self, type, arg, dict): # need to build this up
-		switcher = {"length" : self.compute_length_prob(len(arg), dict )}
+	def type_switch(self, feature, arg, dict): # need to build this up
+	''' feature is the thing we are using to compute a prob.
+		arg is the given text from a cell
+		dict is the type we are in. This function is looped from in classify'''
+		switcher = {"length" : self.compute_feature_prob(len(arg), dict ),
+					"slashes" : self.compute_feature_prob(count_a_char(arg, "/"), dict),
+					"dashes" : self.compute_feature_prob(count_a_char(arg, "-"), dict)
+
+					}
 			
-		return switcher.get(arg, "feature not yet implemented")
+		return switcher.get(feature, "feature not yet implemented")
+	def count_a_char(nText, char):
+		'''A helpful feature for dates probably '''
+		sum = 0
+		for elem in nText:
+			if elem == char
+				sum += 1
 
-	def compute_length_prob(self, test_length, length_dict):
-		if test_length in length_dict:
-			return (length_dict[test_length]) / (sum(length_dict.itervalues()))
+		return sum
+
+	def compute_feature_prob(self, feature, feature_dict):
+		if feature in feature_dict:
+			return (feature_dict[feature]) / (sum(feature_dict.itervalues())) # float this or log this?
 		else:
 			return 0 # or mabye 1
 
