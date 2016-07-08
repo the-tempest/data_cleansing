ad276adfcc9adde5758f5e11021683ec3a8d90ad
Nurik <ntussipov@gmail.com>
2016-07-08 13:13:54 -0500

    added main script

---
+++ main.py
@@ -0,0 +1,14 @@
+import sys, subprocess
+execfile('table.py')
+execfile('column_type.py')
+
+table_name = subprocess.check_output([sys.executable, "extraction.py", sys.argv[1]])
+t = table(table_name);
+
+# call Keith and Pawel's script
+# c = column_typer(t);
+# c.column_typify();
+
+# loop to go through each column to build a JSON to save:
+# for c in t.columns:
+#   
