bdb5e02f5666aa883ad761f132de36f69df415dc
kkravis000 <keithkravis2018@u.northwestern.edu>
2016-07-08 14:40:26 -0500

    changed classifier data members to dictionaries for efficiency

--- typify/classifier.py
+++ typify/classifier.py
@@ -5,16 +5,21 @@ class classifier:
 		2. A list of possible ascii values that can be contained in the type (Ex: names can contain lower_case_letters
 			97-122 but cannot contain '=' 61)
 		3. A list of the known forms that this particular types comes in. (Ex: names can be written as 'Xx Xx' or 'Xx' or 'Xx X. Xx' etc) 
-			I wrote in many known forms but there are plenty that I missed'''
+			I wrote in many known forms but there are plenty that I missed
+		4. A list of a set of known examples for the particular type (Ex: John)
+		5. A list of the known features or common strings that are found in examples of this type (Ex: Dr. or Mr. or II)'''
 	def __init__(self, n, pv, kf, ke, cf):
-		#TODO make all the lists into dictionaries (for time complexity purposes)
 		self.name = n
 		self.possVals = pv
-		self.knownForms = kf
+		self.knownForms = {}
+		for elem in kf:
+			self.knownForms[elem] = 1
 		self.knownExamples = {}
 		for elem in ke:
 			self.knownExamples[elem] = 1
-		self.commonFeatures = cf
+		self.commonFeatures = {}
+		for elem in cf:
+			self.commonFeatures[elem] = 1
 
 	def can_be(self, vals):
 		'''Tests whether the sequence of ascii values inputted has any that are not allowed in this particular type'''
