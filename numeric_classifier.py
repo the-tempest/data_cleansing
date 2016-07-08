002910d9e04d1238e21ba76f0e6a99c7173e9b73
WilliamWallace1272 <williamwallace2018@u.northwestern.edu>
2016-07-08 14:42:23 -0500

    finished the training. Must debug numeric_classifier. Also added method to column

--- numeric_classifier.py
+++ numeric_classifier.py
@@ -14,41 +14,39 @@ class numeric_classifier:
 	def classify(self, nText):
 		type_probabilities = {}
 
-		for type in self.numeric_types: # going to check that it can be of any type
-			type_probabilities[type.name] = 1
-			for feature, feature_occurance in type.feature_dictionary: #for a specific type compute P(feature | type)
+		for t in self.numeric_types: # going to check that it can be of any type
+			type_probabilities[t.name] = 1
+			for feature, feature_occurance in t.feature_dictionary: #for a specific type compute P(feature | type)
 				
 				curr_dictionary = feature_occurance
 				total_number = sum(curr_dictionary.itervalues())
 
 				posterier_type_prob = self.type_switch(feature, nText, curr_dictionary)
 				
-				type_probabilities[type.name] *= posterier_type_prob
+				type_probabilities[t.name] *= posterier_type_prob
 
 		#find the max of all types to guess what the type of the column is
 		max(type_probabilities.iteritems(), key=operator.itemgetter(1))[0] 
 		
 	
 
-	def type_switch(self, feature, arg, dict): # need to build this up
-	''' feature is the thing we are using to compute a prob.
-		arg is the given text from a cell
-		dict is the type we are in. This function is looped from in classify'''
-		switcher = {"length" : self.compute_feature_prob(len(arg), dict ),
-					"slashes" : self.compute_feature_prob(count_a_char(arg, "/"), dict),
-					"dashes" : self.compute_feature_prob(count_a_char(arg, "-"), dict)
-
+	def type_switch(self, feature, arg, curr_dict): # need to build this up
+		''' feature is the thing we are using to compute a prob. arg is the given text from a cell dict is the type we are in. This function is looped from in classify'''
+		switcher = {"length" : self.compute_feature_prob(len(arg), curr_dict ),
+					"slashes" : self.compute_feature_prob(count_a_char(arg, "/"), curr_dict),
+					"dashes" : self.compute_feature_prob(count_a_char(arg, "-"), curr_dict),
+					"decimal points": self.compute_feature_prob(count_a_char(arg, "."), curr_dict)
 					}
 			
 		return switcher.get(feature, "feature not yet implemented") #base case for a feature not yet implemented 
 	def count_a_char(nText, char):
 		'''A helpful feature for dates probably '''
-		sum = 0
+		total = 0
 		for elem in nText:
-			if elem == char
-				sum += 1
+			if elem is char:
+				total += 1
 
-		return sum
+		return total
 
 	def compute_feature_prob(self, feature, feature_dict):
 		if feature in feature_dict:
@@ -68,27 +66,66 @@ class numeric_type:
 		''' Will train given some training data, can edit this later. must give path to directory'''
 
 		training_files_list = [f for f in listdir(training_dir) if isfile(join(training_dir, f))] # gets list of files in teh training _dir
-		for file in training_files_list:
-			file_path = training_dir + file # build up the whole path
+		for training_file in training_files_list:
+			file_path = training_dir + training_file # build up the whole path
 			
 
-			table_name = subprocess.check_output([sys.executable, "extraction.py", file]) #
+			table_name = subprocess.check_output([sys.executable, "extraction.py", training_file]) #
 			t = getTable(table_name) #  returns table object
 			column_names = []
 			for column in t.columns:
 				column_names.append(column.colName)
-			for type in types:
-				if type in column_names:
-					index = t.column_index[type]
-					col = t.column_names[index] # we have the column object now 
-					self.train_type(col)
+			#for type in types:
+				#if type in column_names:
+			for col in column_names:	
+				index = t.column_index[col]
+				column_obj = t.column_names[index] # we have the column object now 
+				self.train_type(column_obj)
 
 	def train_type(self, col):
-		row_list = col.row
-		for item in row_list:
-			if col.colName in self.type_switch_feature_dictionary: #if the type is in the dict
-				
+		row_list = col.rows
 
+		for item in row_list: # each element
+		
+			if col.colName in self.types_feature_dictionary: #if the type is in the dict
 
+				curr_type_dict = self.types_feature_dictionary[col.colName] # dictionary 
+			
+				for feature in self.features: # string of features 
+			
+					if feature in curr_type_dict: # if feature dict exits
+						curr_feature_dict = curr_type_dict[feature]
+						build_feature_freq(curr_feature_dict, item, feature)
+					else:
+						curr_type_dict[feature] = {}
+						build_feature_freq(curr_type_dict[feature], item , feature)
+			else:
+				print " invalid type/column name! FIX HEADER OR CODE"
+						
+	def build_feature_freq(self, curr_dict, nText, feature):
+		feature_result = feature_switch(feature, nText)
+		if feature_result in curr_dict:
+			curr_dict[feature_result] += 1
+		else:
+			curr_dict[feature_result] = 1
 
-trained_date = numeric_type("date")a
+	def feature_switch(self, feature, arg): # need to build this up
+		''' feature is the thing we are using to compute a prob.
+		arg is the given text from a cell
+		dict is the type we are in. This function is looped from in classify'''
+		switcher = {"length" :  len(arg),
+					"slashes" : self.count_a_char(arg, "/"),
+					"dashes" : self.count_a_char(arg, "-")
+
+					}
+			
+		return switcher.get(feature, "feature not yet implemented") #base case for a feature not yet implemented 
+
+	def count_a_char(nText, char):
+		'''A helpful feature for dates probably '''
+		total = 0
+		for elem in nText:
+			if elem == char:
+				total += 1
+
+		return total
