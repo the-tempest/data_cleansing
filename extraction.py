f711b98b843295146a0c6a323fc5a9bb712a83a7
Nurik <ntussipov@gmail.com>
2016-07-08 12:32:33 -0500

    now just extracts file, doesn't call any scripts, returns a name of the new table

--- extraction.py
+++ extraction.py
@@ -1,127 +1,129 @@
 import os, sys, csv, json, mysql.connector
-
-def flatten_json(raw):
-    val = {}
-    for i in raw.keys():
-        if isinstance(raw[i], dict):
-            get = flatten_json(raw[i])
-            for j in get.keys():
-                val[i + "_" + j] = get[j]
-        else:
-            val[i] = raw[i]
-    return val
-
 execfile('column.py');
-execfile('column_type.py');
-# db connector stuff
-cnx = mysql.connector.connect(user='root', password='123', host='localhost', database='world', port='3308')
-cursor = cnx.cursor();
-# sys.argv[1]
-#"./uploaded/SalesJan2009.csv"
-fn, file_extension = os.path.splitext(sys.argv[1]);
-filename = os.path.basename(fn);
-if (file_extension == '.csv'):
-    with open (fn+file_extension, 'rU') as f:
-        reader = csv.reader(f)
-        columns = next(reader)
-        columnsToInitialize = []
-        for col in columns:
-            columnsToInitialize.append(col+ ' TEXT');
-
-        # creating new database
-        query = 'drop table if exists ' + filename;
-        cursor.execute(query)
-
-        # new table and headers
-        query = 'create table ' + filename + '({0})'
-        query = query.format(', '.join(columnsToInitialize))
-        cursor.execute(query)
-
-        s = []
-        query = 'insert into '+ filename +' ({0}) values ({1})'
-        for i in range(len(columns)):
-            s.append('%s')
-        query = query.format(','.join(columns), ','.join(s))
-
-        for data in reader:
-            cursor.execute(query, data)
-
-elif (file_extension == '.json'):
-    with open(fn+file_extension, 'rU') as f:
-        # load and flatten the json file
-        raw_data = json.load(f)
-        data = map(lambda x: flatten_json(x), raw_data)
-        columns = map(lambda x: x.keys(), data)
-        columns = reduce(lambda x,y: x+y, columns)
-        columns = list(set(columns))
-
-        columnsToInitialize = []
-        for col in columns:
-            columnsToInitialize.append(col+ ' TEXT');
+def main(arg):
+    def flatten_json(raw):
+        val = {}
+        for i in raw.keys():
+            if isinstance(raw[i], dict):
+                get = flatten_json(raw[i])
+                for j in get.keys():
+                    val[i + "_" + j] = get[j]
+            else:
+                val[i] = raw[i]
+        return val
 
 
-        # creating new database
-        query = 'drop table if exists ' + filename;
-        cursor.execute(query)
+    # db connector stuff
+    cnx = mysql.connector.connect(user='root', password='123', host='localhost', database='world', port='3308')
+    cursor = cnx.cursor();
+    # sys.argv[1]
+    #"./uploaded/SalesJan2009.csv"
+    fn, file_extension = os.path.splitext(arg);
+    filename = os.path.basename(fn);
+    if (file_extension == '.csv'):
+        with open (fn+file_extension, 'rU') as f:
+            reader = csv.reader(f)
+            columns = next(reader)
+            columnsToInitialize = []
+            for col in columns:
+                columnsToInitialize.append(col+ ' TEXT');
 
-        # new table and headers
-        query = 'create table ' + filename + '({0})'
-        query = query.format(', '.join(columnsToInitialize))
-        cursor.execute(query)
+            # creating new database
+            query = 'drop table if exists ' + filename;
+            cursor.execute(query)
 
-        s = []
-        query = 'insert into '+ filename +' ({0}) values ({1})'
-        for i in range(len(columns)):
-            s.append('%s')
-        query = query.format(','.join(columns), ','.join(s))
+            # new table and headers
+            query = 'create table ' + filename + '({0})'
+            query = query.format(', '.join(columnsToInitialize))
+            cursor.execute(query)
 
-        for item in data:
-            new_item = []
-            for key in item:
-                new_item.append(str(item[key]))
-            cursor.execute(query, new_item);
+            s = []
+            query = 'insert into '+ filename +' ({0}) values ({1})'
+            for i in range(len(columns)):
+                s.append('%s')
+            query = query.format(','.join(columns), ','.join(s))
 
-columnTypePairs = {};
+            for data in reader:
+                cursor.execute(query, data)
 
-def getRows(columnName):
-    query = "SELECT " + columnName + " FROM " + filename;
-    cursor.execute(query);
-    rows = cursor.fetchall();
-    for j in range(len(rows)):
-        rows[j] = str(''.join(rows[j]));
-    return rows;
+    elif (file_extension == '.json'):
+        with open(fn+file_extension, 'rU') as f:
+            # load and flatten the json file
+            raw_data = json.load(f)
+            data = map(lambda x: flatten_json(x), raw_data)
+            columns = map(lambda x: x.keys(), data)
+            columns = reduce(lambda x,y: x+y, columns)
+            columns = list(set(columns))
 
-def classify(col):
-    ''' takes a column object and runs Keith's script on it '''
-    coltyper = column_typer(col);
-    t = coltyper.column_typify(); #type
-    columnTypePairs[columns[i]] = t;
+            columnsToInitialize = []
+            for col in columns:
+                columnsToInitialize.append(col+ ' TEXT');
 
 
-rows = None;
-nextCol = getRows(columns[0]);
+            # creating new database
+            query = 'drop table if exists ' + filename;
+            cursor.execute(query)
 
-for i in range (len(columns)-1):
-    prevCol = rows;
-    rows = nextCol;
-    nextCol = getRows(columns[i+1]);
-    colName = columns[i];
-    col = column(rows, colName, prevCol, nextCol);
+            # new table and headers
+            query = 'create table ' + filename + '({0})'
+            query = query.format(', '.join(columnsToInitialize))
+            cursor.execute(query)
+
+            s = []
+            query = 'insert into '+ filename +' ({0}) values ({1})'
+            for i in range(len(columns)):
+                s.append('%s')
+            query = query.format(','.join(columns), ','.join(s))
+
+            for item in data:
+                new_item = []
+                for key in item:
+                    new_item.append(str(item[key]))
+                cursor.execute(query, new_item);
+
+    #columnTypePairs = {};
+
+
+
+    #def classify(col):
+        ''' takes a column object and runs Keith's script on it '''
+        '''coltyper = column_typer(col)
+        t = coltyper.column_typify() #type
+        columnTypePairs[columns[i]] = t'''
+
+
+
+
+
+    '''rows = None;
+    nextCol = getRows(columns[0]);
+
+    for i in range (len(columns)-1):
+        prevCol = rows;
+        rows = nextCol;
+        nextCol = getRows(columns[i+1]);
+        colName = columns[i];
+        col = column(rows, colName, prevCol, nextCol);
+        #classify(col);
+
+    col = column(nextCol, columns[i+1], rows, None);
     #classify(col);
 
-col = column(nextCol, columns[i+1], rows, None);
-#classify(col);
+
+    # for testing
+    #columnTypePairs = {"city" : "location", "Name": "name"} ;
+
+    ##with open('output/columnTypes.txt', 'w') as outfile:
+    #    json.dump(columnTypePairs, outfile);
+
+    '''
 
 
-# for testing
-#columnTypePairs = {"city" : "location", "Name": "name"} ;
+    cnx.commit()
+    cursor.close()
+    cnx.close()
+    return filename;
 
-##with open('output/columnTypes.txt', 'w') as outfile:
-#    json.dump(columnTypePairs, outfile);
-
-
-
-
-cnx.commit()
-cursor.close()
-cnx.close()
+if __name__ == "__main__":
+    x = main(sys.argv[1])
+    print x;
