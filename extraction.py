85962a3b5b529c57cb388f401382e64826b8e4f1
WilliamWallace1272 <williamwallace2018@u.northwestern.edu>
2016-07-11 12:05:02 -0500

    work on bug changes

--- extraction.py
+++ extraction.py
@@ -14,7 +14,7 @@ def main(arg):
 
 
     # db connector stuff
-    cnx = mysql.connector.connect(user='root', password='123', host='localhost', database='world', port='3308')
+    cnx = mysql.connector.connect(user='root', password='spence23', host='localhost', database='world')
     cursor = cnx.cursor();
     # sys.argv[1]
     #"./uploaded/SalesJan2009.csv"
