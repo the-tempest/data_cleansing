de0b602f04d6bcda7d991a83ef69c75d40d99e1a
kkravis000 <keithkravis2018@u.northwestern.edu>
2016-07-11 15:24:37 -0500

    Made changes to support regular expression functionality in classifier.py

--- typify/classifier.py
+++ typify/classifier.py
@@ -1,20 +1,18 @@
+import re
+
 class classifier:
 	'''this class is a class used only in the column_classifiers data member in order to structure the types better.
 	The class contains:
 		1. A name of the type (Ex: 'names')
 		2. A list of possible ascii values that can be contained in the type (Ex: names can contain lower_case_letters
 			97-122 but cannot contain '=' 61)
-		3. A list of the known forms that this particular types comes in. (Ex: names can be written as 'Xx Xx' or 'Xx' or 'Xx X. Xx' etc) 
-			I wrote in many known forms but there are plenty that I missed
+		3. A regular expression representing the form of the type
 		4. A list of a set of known examples for the particular type (Ex: John)
 		5. A list of the known features or common strings that are found in examples of this type (Ex: Dr. or Mr. or II)'''
-	# TODO change all list to dictionaries, so it's constant time
-	def __init__(self, n, pv, kf, ke, cf):
+	def __init__(self, n, pv, re, ke, cf):
 		self.name = n
 		self.possVals = pv
-		self.knownForms = {}
-		for elem in kf:
-			self.knownForms[elem] = 1
+		self.regEx = re
 		self.knownExamples = {}
 		for elem in ke:
 			self.knownExamples[elem] = 1
@@ -29,9 +27,11 @@ class classifier:
 				return False
 		return True
 
-	def has_form(self, form):
-		'''Tests whether the given condensed form is in the known forms of this particular type'''
-		return form in self.knownForms
+	def has_form(self, token):
+		'''Tests whether the given token is in the form of the regular expression for the type'''
+		if len(re.findall(self.regEx, token)) == 1:
+			return True
+		return False
 
 	def is_a(self, inString):
 		'''Tests whether the given string is stored in the list of known string examples of the particular types'''
