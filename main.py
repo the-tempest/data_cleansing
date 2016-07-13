14cedf2c2b2da0272bc4d14db84e90977ad1fd37
Nurik <ntussipov@gmail.com>
2016-07-13 13:16:05 -0500

    calls both scripts and saves them now

--- main.py
+++ main.py
@@ -5,10 +5,11 @@ execfile('numeric_classifier.py')
 #execfile('typify/column_type.py')
 
 table_name = subprocess.check_output([sys.executable, "extraction.py", sys.argv[1]])
+table_name = table_name.replace("\n", "")
 t = getTable(table_name);
 #t.build_column_index();
 
-'''numClass = numeric_classifier();
+numClass = numeric_classifier();
 result = "";
 for col in t.columns:
     diction = {}
@@ -19,7 +20,7 @@ for col in t.columns:
         else:
             diction[res] = 1;
     result += col.colName + ': ' + max(diction.iteritems(), key=operator.itemgetter(1))[0];
-    result += os.linesep;'''
+    result += '\n'
 
 
 
@@ -27,8 +28,15 @@ for col in t.columns:
 c = column_typer(t);
 cl = c.build_report();
 
-with open('output/' + table_name + '.txt', 'w') as outfile:
-    json.dump(cl, outfile);
+#with open('output/' + table_name + '.txt', 'w') as outfile:
+#    json.dump(cl, outfile);
+
+with open("output/" + table_name + '_general.txt', "w") as text_file:
+    text_file.write(cl);
+
+with open("output/" + table_name + '_numeric.txt', "w") as text_file:
+    text_file.write(result);
+
 
 # loop to go through each column to build a JSON to save:
 # for c in t.columns:
