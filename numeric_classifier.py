8588c7d151e787a96591edef29f82ee5683a4a86
WilliamWallace1272 <williamwallace2018@u.northwestern.edu>
2016-07-14 14:21:08 -0500

    made numeric_classifier a little prettier

--- numeric_classifier.py
+++ numeric_classifier.py
@@ -8,32 +8,45 @@ import time
 execfile('table.py')
 training_directory = r"uploaded/numeric_training_data"
 
-features = ['length', 'slashes', 'dashes', 'spaces', 'decimal points']
+features = ['length', 'slashes', 'dashes', 'spaces', 'decimal points'] # default features and types
 types = ['Date', 'Longitude', 'Latitude', 'Number', 'Zip', 'Phone_Number', 'IP']
 
+''' Form of the feature_dictionary that gets built in train and is used to classify
+{'Phone_Number': {'slashes': {0: 1000}, 
+				'length': {16: 635, 17: 277, 15: 88}, 
+				'spaces': {0: 1000}, 
+				'decimal points': {0: 1000}, 
+				'dashes': {2: 1000}}, 
+'Zip': {
+	'slashes': {0: 998}, 
+	'length': {4: 36, 5: 461, 6: 1, 8: 7, 9: 40, 10: 453}, 
+	'spaces': {0: 998}, 
+	'decimal points': {0: 998}, 
+	'dashes': {0: 545, 1: 453}}} 
+'''
 
 class numeric_classifier:
 	def __init__(self, numeric_types = types, features = features):
 		self.features = features
 		self.numeric_types = numeric_types #list of all numeric_type classes (strings)
 
-		if os.path.isfile("types_feature_dictionary.dat"):
-			self.feature_dictionary = self.load("types_feature_dictionary.dat")
+		if os.path.isfile("training_dictionary.dat"):
+			self.training_dictionary = self.load("training_dictionary.dat")
 		else:
 			trainer = numeric_trainer(self.numeric_types, self.features)
 			trainer.train(training_directory)
-			self.feature_dictionary = trainer.types_feature_dictionary
+			self.training_dictionary = trainer.training_dictionary
 
 
 	def classify(self, nText):
+		'''Takes in a string and classifies it to one of the numeric_classifiers types '''
 		type_probabilities = {}
 
-		for t in self.numeric_types: # going to check that it can be of any type
-			type_probabilities[t] = 1
+		for t in self.numeric_types: 
+			type_probabilities[t] = 1 #initialize 
 			for feature in self.feature_dictionary[t]: #for a specific type compute P(feature | type)
-				#print feature
+				
 				curr_dictionary = self.feature_dictionary[t][feature]
-				total_number = sum(curr_dictionary.itervalues())
 
 				posterier_type_prob = self.type_switch(feature, nText, curr_dictionary)
 
@@ -41,29 +54,20 @@ class numeric_classifier:
 
 		#find the max of all types to guess what the type of the column is
 		result = max(type_probabilities.iteritems(), key=operator.itemgetter(1))[0]
-		#print type_probabilities
-		#print result
+		
 		return result
 
 
 	def type_switch(self, feature, arg, curr_dict): # need to build this up
 		''' feature is the thing we are using to compute a prob. arg is the given text from a cell dict is the type we are in. This function is looped from in classify'''
 		switcher = {"length" : self.compute_feature_prob(len(arg), curr_dict ),
-					"slashes" : self.compute_feature_prob(self.count_a_char(arg, "/"), curr_dict),
-					"dashes" : self.compute_feature_prob(self.count_a_char(arg, "-"), curr_dict),
-					"decimal points": self.compute_feature_prob(self.count_a_char(arg, "."), curr_dict),
-					"spaces": self.compute_feature_prob(self.count_a_char(arg, " "), curr_dict)
+					"slashes" : self.compute_feature_prob(arg.count("/"), curr_dict),
+					"dashes" : self.compute_feature_prob(arg.count("-"), curr_dict),
+					"decimal points": self.compute_feature_prob(arg.count("."), curr_dict),
+					"spaces": self.compute_feature_prob(arg.count(" "), curr_dict)
 					}
 
 		return switcher.get(feature, "feature not yet implemented") #base case for a feature not yet implemented
-	def count_a_char(self, nText, char):
-		'''A helpful feature for dates probably '''
-		total = 0
-		for elem in nText:
-			if elem is char:
-				total += 1
-
-		return total
 
 	def compute_feature_prob(self, feature, feature_dict):
 		if feature in feature_dict:
@@ -80,25 +84,24 @@ class numeric_classifier:
 		return dObj
 
 
-class numeric_trainer:
+class numeric_trainer: # class fo holding training functions
 	def __init__(self, types = [], features =[]):
 		self.features = features
 		self.types = types
-		self.types_feature_dictionary = {} # a dictionary of feature dictionaries
+		self.training_dictionary = {} # a dictionary of feature dictionaries
 
 
 	def train(self, training_dir):
 		''' Will train given some training data, can edit this later. must give path to directory
 				need to put r in front of training_dir for windows at least'''
 
-		#first need to initialize types_feature_dictionary
-
+		
 		training_files_list = [f for f in listdir(training_dir) if isfile(join(training_dir, f))] # gets list of files in teh training _dir
 		for training_file in training_files_list:
 
 			file_path = os.path.join(training_dir, training_file) # build up the whole path
 			print file_path + "\n"
-			table_name = subprocess.check_output([sys.executable, "extraction.py", file_path]) #
+			table_name = subprocess.check_output([sys.executable, "extraction.py", file_path]) 
 			t = getTable(table_name, "root", "123", "localhost", "world") #  returns table object
 			column_names = []
 
@@ -108,30 +111,27 @@ class numeric_trainer:
 				for x in range(10):
 					column.colName = column.colName.replace(chr(ord(firstNum) + x), "")
 
-				t.build_column_index()
+				t.build_column_index() #builds up index for easy access
 
 				if column.colName in self.types: #building the columns we are going to train as long as they are types we want
 					column_names.append(column.colName)
 
-
-
 			for col in column_names:
 				index = t.column_index[col]
 				column_obj = t.columns[index] # we have the column object now
-				self.train_type(column_obj)
+				self.train_on_column(column_obj)
 
-		#self.save(self.types_feature_dictionary, "types_feature_dictionary.dat")
+		#self.save(self.training_dictionary, "training_dictionary.dat")
 
-	def train_type(self, col):
+	def train_on_column(self, col):
 		row_list = col.rows
 
-		for item in row_list: # each element
-			if item == "NULL":
+		for item in row_list: # each cell in a column's row list
+			if item == "NULL": # workaround for now for empty cells
 				continue
-			#print item
-			if col.colName in self.types_feature_dictionary: #if the type is in the dict
+			if col.colName in self.training_dictionary: #if the type is in the dict
 
-				curr_type_dict = self.types_feature_dictionary[col.colName] # dictionary
+				curr_type_dict = self.training_dictionary[col.colName] # get dictionary of feature dictionaries
 
 				for feature in self.features: # string of features
 
@@ -142,8 +142,8 @@ class numeric_trainer:
 						curr_type_dict[feature] = {}
 						self.build_feature_freq(curr_type_dict[feature], item , feature)
 			else:
-				self.types_feature_dictionary[col.colName] = {}
-				curr_type_dict = self.types_feature_dictionary[col.colName]
+				self.training_dictionary[col.colName] = {}
+				curr_type_dict = self.training_dictionary[col.colName]
 
 				for feature in self.features:
 
@@ -153,8 +153,6 @@ class numeric_trainer:
 					else:
 						curr_type_dict[feature] = {}
 						self.build_feature_freq(curr_type_dict[feature], item , feature)
-
-				#print " invalid type/column name! FIX HEADER OR CODE"
 
 	def build_feature_freq(self, curr_dict, nText, feature):
 		feature_result = self.feature_switch(feature, nText)
@@ -168,32 +166,17 @@ class numeric_trainer:
 		arg is the given text from a cell
 		dict is the type we are in. This function is looped from in classify'''
 		switcher = {"length" :  len(arg),
-					"slashes" : self.count_a_char(arg, "/"),
-					"dashes" : self.count_a_char(arg, "-"),
-					"decimal points" : self.count_a_char(arg, "."),
-					"spaces": self.count_a_char(arg, " ")
+					"slashes" : arg.count("/"),
+					"dashes" : arg.count("-"),
+					"decimal points" : arg.count("."),
+					"spaces": arg.count(" ")
 					}
 
 		return switcher.get(feature, "feature not yet implemented") #base case for a feature not yet implemented
 
-	def count_a_char(self, nText, char):
-		'''A helpful feature for dates probably '''
-		total = 0
-		for elem in nText:
-			if elem == char:
-				total += 1
-
-		return total
-
-
-	def save(self, dObj, sFilename):
+	def save(self, dObj, sFilename): # save dictionary with pickle
 		f = open(sFilename, "w")
 		p = pickle.Pickler(f)
 		p.dump(dObj)
 		f.close()
-		# """Given an object and a file name, write the object to the file using pickle."""
-
-  #       f = open(sFilename, "w")
-  #       p = pickle.Pickler(f)
-  #       p.dump(dObj)
-  #       f.close()
+		
