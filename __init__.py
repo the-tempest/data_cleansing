c63ce2f0afc008d6151f8710d781b9971dc2872d
Nurik <ntussipov@gmail.com>
2016-07-25 20:11:43 -0500

    server now only processes csv and json

--- __init__.py
+++ __init__.py
@@ -8,21 +8,21 @@ import main
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = path+'uploaded/'
 app.config['OUTPUT_FOLDER'] = path+'output/'
-app.config['COOKIE LIST'] = {};
+app.config['ALLOWED_EXT'] = set(['csv','json'])
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in app.config['ALLOWED_EXT']
 
 @app.route('/')
 def index():
     return  app.send_static_file('index.html')
 
-
-#@app.route('/profile/<name>')
-#def profile(name):
-#    return render_template("profile.html", namePassed=name); # passes name as arg to html template
 @app.route('/process', methods=['POST'])
 def process():
     for i in range(len(request.files)):
         f = request.files['file'+str(i)]
-        if f:
+        if f and allowed_file(f.filename):
             now = datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
             noExt, ext = os.path.splitext(f.filename);
             filename = os.path.join(app.config['UPLOAD_FOLDER'], noExt+now+ext)
@@ -34,6 +34,8 @@ def process():
             #app.config['COOKIE LIST'][]
             #r.set_cookie('username', k)
             return r;
+        else:
+            return "wrong file extension";
 
 
     return "No files were uploaded";
