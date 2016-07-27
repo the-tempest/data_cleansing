db1b223491e097349b4e922f4a2d57a6642dbd33
WilliamWallace1272 <williamwallace2018@u.northwestern.edu>
2016-07-27 10:31:04 -0500

    #fixtheserver

--- main.py
+++ main.py
@@ -31,7 +31,6 @@ def execute(filename):
     # call Keith and Pawel's script
     c = column_typer(t);
     cl = c.build_report();
-    ///14asdfadf = 1asdf 
     
 
     #with open('output/' + table_name + '.txt', 'w') as outfile:
