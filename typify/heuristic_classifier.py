2206f60f43c134eb80406aaadd5467c25f4d1371
parwell <pawelklim1@gmail.com>
2016-08-01 13:50:01 -0500

    continued creating an overall classifier

---
+++ typify/heuristic_classifier.py
@@ -0,0 +1,99 @@
+# heuristic_classifier.py
+# this defines a heuristic_classifier class which
+# takes in a token and returns a prediction
+# based on regular expressions and such
+
+import mysql.connector, os, re
+from secrets import path
+execfile(path+"typify/heuristics.py")
+execfile(path+"typify/helper.py")
+execfile(path+"typify/features/features.py")
+execfile(path+"typify/classifier.py")
+execfile(path+'table.py')
+
+ASCII_NUMS = [n for n in range(48, 58)]
+ASCII_UPPER = [n for n in range(65, 91)]
+ASCII_LOWER = [n for n in range(97, 123)]
+ASCII_PRINTABLE = [n for n in range(32, 128)]
+ASCII_ADDRESS = [32, 39, 44, 45, 46] + ASCII_NUMS + [58, 59] + ASCII_UPPER + ASCII_LOWER
+ASCII_NAME = [32, 44, 45, 46] + ASCII_UPPER + ASCII_LOWER
+
+
+class heuristic_classifier:
+	def __init__(self):
+		self.build_heuristic_classifiers()
+
+	def classify(self, token):
+		'''takes in a token and returns
+		a list of predictions'''
+		certainties = {}
+		for f in heuristics:
+			tipe, value = f(token, self)
+			certainties[tipe] = value
+		top_score = dict_max(certainties)
+		prediction = [x for x in certainties if x == top_score]
+		return prediction
+
+	def build_heuristic_classifiers(self):
+		'''builds the self.column_classifiers data member by creating classifier objects created
+		by classifier(name of the type, possible ascii values in the type string, list of the known condensed forms)'''
+		# TODO complete classifiers
+		self.heuristic_classifiers = []
+
+		# names
+		names = ['full name', 'first name', 'last name', 'datestring',
+				'full address', 'street address', 'city state', 'email',
+				'location', 'description', 'url', 'city', 'state']
+
+		# possible values
+		datestring_pv = [32, 44, 46] + ASCII_NUMS + ASCII_UPPER + ASCII_LOWER
+		email_pv = [43, 45, 46, 64, 95] + ASCII_NUMS + ASCII_UPPER + ASCII_LOWER
+		description_pv = ASCII_PRINTABLE
+		url_punc = [33, 58, 59, 61, 63, 91, 93, 95, 126] + [x for x in range(35, 48)]
+		url_pv = ASCII_LOWER + ASCII_UPPER + ASCII_NUMS + url_punc
+		possible_values = [ASCII_NAME, ASCII_NAME, ASCII_NAME, datestring_pv,
+					 ASCII_ADDRESS, ASCII_ADDRESS, ASCII_NAME, email_pv,
+					 ASCII_NAME, description_pv, url_pv, ASCII_NAME, ASCII_NAME]
+		# TODO better city and state pv
+
+		# regular expressions
+		fn_regex = r'''^[-.a-zA-Z']*?,?\s(?:[-a-zA-Z']*\.?\s)*?[-a-zA-Z']*\.?$'''
+		na_regex = r'''^[A-Z][a-z'-]*$'''
+		ds_regex = r'''^(?:[A-Z][a-zA-Z]*\.?,?\s)?(?:[0-3][0-9]\s)?[A-Z][a-zA-Z]*\.?,?\s(?:[0-3][0-9]\.?,?\s)?[0-9]*$'''
+		fa_regex = r'''^(?:[Oo][Nn][Ee]|[0-9-]*[a-zA-Z]?)\s+(?:[NSEW]\.?[NSEW]?\.?\s+|(?:[NSEWnsew][OAEoae][RUSrus][Tt][Hh]?|[NSns][Oo][RUru][Tt][Hh][EWew][EAea][Ss][Tt])\s+)?(?:\d*(?:[SNRTsnrt][TDHtdh])?(?:\s+[a-zA-Z'-]*)?|(?:[a-zA-Z'-]*\s+)*?(?:[a-zA-Z'-]*))\.?(?:\s+\d*)?,?(?:\s+[NSEW]\.?[NESW]?\.?|\s+(?:[NSEWnsew][OAEoae][RUSrus][Tt][Hh]?|[NSns][Oo][RUru][Tt][Hh][EWew][EAea][Ss][Tt])?)?(?:\s+\d*(?:[SNRTsnrt][TDHtdh])?\s+[a-zA-Z]*\.?|\s+(?:[a-zA-Z][a-z]*\.?|[Pp][Oo]\.?\s+?[Bb][Oo][Xx])?(?:\s+(?:[#]\s*)?\w*(?:[-/: ]\w*)?))?,?\s(?:[a-zA-Z'-]*\s)*?[a-zA-Z'-]*,?\s[a-zA-Z]*,?\s(?:\d{5}|\d{5}(?:\s|[.-])?\d{4})(?:,?\s[A-Za-z'-]*)*$'''
+		sa_regex = r'''^(?:[Oo][Nn][Ee]|[0-9-]*[a-zA-Z]?)\s+(?:[NSEW]\.?[NSEW]?\.?\s+|(?:[NSEWnsew][OAEoae][RUSrus][Tt][Hh]?|[NSns][Oo][RUru][Tt][Hh][EWew][EAea][Ss][Tt])\s+)?(?:\d*(?:[SNRTsnrt][TDHtdh])?(?:\s+[a-zA-Z'-]*)?|(?:[a-zA-Z'-]*\s+)*?(?:[a-zA-Z'-]*))\.?(?:\s+\d*)?,?(?:\s+[NSEW]\.?[NESW]?\.?|\s+(?:[NSEWnsew][OAEoae][RUSrus][Tt][Hh]?|[NSns][Oo][RUru][Tt][Hh][EWew][EAea][Ss][Tt])?)?(?:\s+\d*(?:[SNRTsnrt][TDHtdh])?\s+[a-zA-Z]*\.?|\s+(?:[a-zA-Z][a-z]*\.?|[Pp][Oo]\.?\s+?[Bb][Oo][Xx])?(?:\s+(?:[#]\s*)?\w*(?:[-/: ]\w*)?))?$'''
+		cs_regex = r'''^(?:[a-zA-Z'-]*\s)*?[a-zA-Z'-]*,?\s[a-zA-Z]*$'''
+		em_regex = r'''^\S*?@\S*?(?:\.\S*?)+$'''
+		lo_regex = r'''^(?:[A-Z][a-z'-]*\s)*?(?:[A-Z][a-z'-]*)$'''
+		de_regex = r'''^(?:["'<-]?[A-Za-z0-9'-]+[>"',;:-]?(?:\s|[.?!]\s+))+$'''
+		ur_regex = r'''^\S*?.\S*'''
+		regex = [fn_regex, na_regex, na_regex, ds_regex,
+				fa_regex, sa_regex, cs_regex, em_regex,
+				lo_regex, de_regex, ur_regex, na_regex, na_regex]
+		# TODO better regex for city and state
+
+
+		# known examples
+		full_name_ex      = COMMON_PREFIXES + COMMON_SUFFIXES + COMMON_FIRST_NAMES + COMMON_LAST_NAMES
+		first_name_ex     = COMMON_PREFIXES + COMMON_FIRST_NAMES
+		last_name_ex      = COMMON_SUFFIXES + COMMON_LAST_NAMES
+		datestring_ex     = COMMON_DATE_NAMES + COMMON_DATE_ABBREV
+		full_address_ex   = COMMON_ADDRESS_NAMES + COMMON_ADDRESS_FEATURES + COMMON_STATEPROV_ABBREV + COMMON_CITIES
+		street_address_ex = COMMON_ADDRESS_NAMES + COMMON_ADDRESS_FEATURES
+		city_state_ex     = COMMON_STATEPROV_ABBREV + COMMON_CITIES
+		email_ex          = COMMON_URL_EXTENSIONS + COMMON_EMAIL_DOMAINS
+		location_ex       = COMMON_CITIES + COMMON_LOCATION_FEATURES
+		description_ex    = COMMON_ADJECTIVES
+		url_ex            = COMMON_URL_EXTENSIONS + COMMON_URL
+		city_ex           = COMMON_CITIES
+		state_ex          = COMMON_STATES
+		known_examples = [full_name_ex, first_name_ex, last_name_ex, datestring_ex,
+						  full_address_ex, street_address_ex, city_state_ex, email_ex,
+						  location_ex, description_ex, url_ex, city_ex, state_ex]
+
+		for i in range(len(names)):
+			curr = classifier(names[i],
+							  possible_values[i],
+							  regex[i],
+							  known_examples[i])
+			self.heuristic_classifiers.append(curr)
--- typify/main_classifier.py
+++ typify/main_classifier.py
@@ -14,18 +14,11 @@ execfile(path+'numeric_classifier.py')
 execfile(path+'table.py')
 execfile(path+"typify/tie_breaker.py")
 
-ASCII_NUMS = [n for n in range(48, 58)]
-ASCII_UPPER = [n for n in range(65, 91)]
-ASCII_LOWER = [n for n in range(97, 123)]
-ASCII_PRINTABLE = [n for n in range(32, 128)]
-ASCII_ADDRESS = [32, 39, 44, 45, 46] + ASCII_NUMS + [58, 59] + ASCII_UPPER + ASCII_LOWER
-ASCII_NAME = [32, 44, 45, 46] + ASCII_UPPER + ASCII_LOWER
-
 class main_classifier:
 	def __init__(self):
 		self.build_classifiers()
-		self.num_class = numeric_classifier()
-		self.heu_class = heuristic_classifier()
+		self.naivebayes_class = naivebayes_classifier()
+		self.heuristic_class = heuristic_classifier()
 
 		# the data that is used by the classifier
 		self.my_table     = None
@@ -87,10 +80,10 @@ class main_classifier:
 		have been misclassfied as their classification is 
 		not the plurality type'''
 		think = column.tentClass
-		dict = column.guesses
+		dyct = column.guesses
 		error_list = []
-		for i in range(len(dict)):
-			if dict[i]!=think:
+		for i in range(len(dyct)):
+			if dyct[i]!=think:
 				error_list.append(i)
 		return error_list
 			
@@ -175,84 +168,24 @@ class main_classifier:
 			dyct[i]= guess
 			i = i+1
 			predictions.append(guess)
-			#print dyct
 		return predictions, dyct
 
 	def token_typify(self, token):
 		'''takes in a token and returns a
 		prediction for its type'''
-		# TODO add naice bayes
-		if no_letters(token):
-			tipe, probability_dictionary, mean, std_dev = self.numClass.classify(token)
-			#print tipe
-			return tipe
-		certainties = {}
-		for f in heuristics:
-			tipe, value = f(token, self)
-			certainties[tipe] = value
-		prediction = dict_max(certainties)
-		return prediction
+		# TODO finish implementing
+		
+		# Naive Bayes part
+		# returns this tuple
+		# (guess, probability_dictionary, mean, std_dev)
+		prediction1 = self.naivebayes_classifier.classify(token)[0]
 
-	def build_heuristic_classifiers(self):
-		'''builds the self.column_classifiers data member by creating classifier objects created
-		by classifier(name of the type, possible ascii values in the type string, list of the known condensed forms)'''
-		# TODO complete classifiers
-		self.heuristic_classifiers = []
-
-		# names
-		names = ['full name', 'first name', 'last name', 'datestring',
-				'full address', 'street address', 'city state', 'email',
-				'location', 'description', 'url', 'city', 'state']
-
-		# possible values
-		datestring_pv = [32, 44, 46] + ASCII_NUMS + ASCII_UPPER + ASCII_LOWER
-		email_pv = [43, 45, 46, 64, 95] + ASCII_NUMS + ASCII_UPPER + ASCII_LOWER
-		description_pv = ASCII_PRINTABLE
-		url_punc = [33, 58, 59, 61, 63, 91, 93, 95, 126] + [x for x in range(35, 48)]
-		url_pv = ASCII_LOWER + ASCII_UPPER + ASCII_NUMS + url_punc
-		possible_values = [ASCII_NAME, ASCII_NAME, ASCII_NAME, datestring_pv,
-					 ASCII_ADDRESS, ASCII_ADDRESS, ASCII_NAME, email_pv,
-					 ASCII_NAME, description_pv, url_pv, ASCII_NAME, ASCII_NAME]
-		# TODO better city and state pv
-
-		# regular expressions
-		fn_regex = r'''^[-.a-zA-Z']*?,?\s(?:[-a-zA-Z']*\.?\s)*?[-a-zA-Z']*\.?$'''
-		na_regex = r'''^[A-Z][a-z'-]*$'''
-		ds_regex = r'''^(?:[A-Z][a-zA-Z]*\.?,?\s)?(?:[0-3][0-9]\s)?[A-Z][a-zA-Z]*\.?,?\s(?:[0-3][0-9]\.?,?\s)?[0-9]*$'''
-		fa_regex = r'''^(?:[Oo][Nn][Ee]|[0-9-]*[a-zA-Z]?)\s+(?:[NSEW]\.?[NSEW]?\.?\s+|(?:[NSEWnsew][OAEoae][RUSrus][Tt][Hh]?|[NSns][Oo][RUru][Tt][Hh][EWew][EAea][Ss][Tt])\s+)?(?:\d*(?:[SNRTsnrt][TDHtdh])?(?:\s+[a-zA-Z'-]*)?|(?:[a-zA-Z'-]*\s+)*?(?:[a-zA-Z'-]*))\.?(?:\s+\d*)?,?(?:\s+[NSEW]\.?[NESW]?\.?|\s+(?:[NSEWnsew][OAEoae][RUSrus][Tt][Hh]?|[NSns][Oo][RUru][Tt][Hh][EWew][EAea][Ss][Tt])?)?(?:\s+\d*(?:[SNRTsnrt][TDHtdh])?\s+[a-zA-Z]*\.?|\s+(?:[a-zA-Z][a-z]*\.?|[Pp][Oo]\.?\s+?[Bb][Oo][Xx])?(?:\s+(?:[#]\s*)?\w*(?:[-/: ]\w*)?))?,?\s(?:[a-zA-Z'-]*\s)*?[a-zA-Z'-]*,?\s[a-zA-Z]*,?\s(?:\d{5}|\d{5}(?:\s|[.-])?\d{4})(?:,?\s[A-Za-z'-]*)*$'''
-		sa_regex = r'''^(?:[Oo][Nn][Ee]|[0-9-]*[a-zA-Z]?)\s+(?:[NSEW]\.?[NSEW]?\.?\s+|(?:[NSEWnsew][OAEoae][RUSrus][Tt][Hh]?|[NSns][Oo][RUru][Tt][Hh][EWew][EAea][Ss][Tt])\s+)?(?:\d*(?:[SNRTsnrt][TDHtdh])?(?:\s+[a-zA-Z'-]*)?|(?:[a-zA-Z'-]*\s+)*?(?:[a-zA-Z'-]*))\.?(?:\s+\d*)?,?(?:\s+[NSEW]\.?[NESW]?\.?|\s+(?:[NSEWnsew][OAEoae][RUSrus][Tt][Hh]?|[NSns][Oo][RUru][Tt][Hh][EWew][EAea][Ss][Tt])?)?(?:\s+\d*(?:[SNRTsnrt][TDHtdh])?\s+[a-zA-Z]*\.?|\s+(?:[a-zA-Z][a-z]*\.?|[Pp][Oo]\.?\s+?[Bb][Oo][Xx])?(?:\s+(?:[#]\s*)?\w*(?:[-/: ]\w*)?))?$'''
-		cs_regex = r'''^(?:[a-zA-Z'-]*\s)*?[a-zA-Z'-]*,?\s[a-zA-Z]*$'''
-		em_regex = r'''^\S*?@\S*?(?:\.\S*?)+$'''
-		lo_regex = r'''^(?:[A-Z][a-z'-]*\s)*?(?:[A-Z][a-z'-]*)$'''
-		de_regex = r'''^(?:["'<-]?[A-Za-z0-9'-]+[>"',;:-]?(?:\s|[.?!]\s+))+$'''
-		ur_regex = r'''^\S*?.\S*'''
-		regex = [fn_regex, na_regex, na_regex, ds_regex,
-				fa_regex, sa_regex, cs_regex, em_regex,
-				lo_regex, de_regex, ur_regex, na_regex, na_regex]
-		# TODO better regex for city and state
-
-
-		# known examples
-		full_name_ex      = COMMON_PREFIXES + COMMON_SUFFIXES + COMMON_FIRST_NAMES + COMMON_LAST_NAMES
-		first_name_ex     = COMMON_PREFIXES + COMMON_FIRST_NAMES
-		last_name_ex      = COMMON_SUFFIXES + COMMON_LAST_NAMES
-		datestring_ex     = COMMON_DATE_NAMES + COMMON_DATE_ABBREV
-		full_address_ex   = COMMON_ADDRESS_NAMES + COMMON_ADDRESS_FEATURES + COMMON_STATEPROV_ABBREV + COMMON_CITIES
-		street_address_ex = COMMON_ADDRESS_NAMES + COMMON_ADDRESS_FEATURES
-		city_state_ex     = COMMON_STATEPROV_ABBREV + COMMON_CITIES
-		email_ex          = COMMON_URL_EXTENSIONS + COMMON_EMAIL_DOMAINS
-		location_ex       = COMMON_CITIES + COMMON_LOCATION_FEATURES
-		description_ex    = COMMON_ADJECTIVES
-		url_ex            = COMMON_URL_EXTENSIONS + COMMON_URL
-		city_ex           = COMMON_CITIES
-		state_ex          = COMMON_STATES
-		known_examples = [full_name_ex, first_name_ex, last_name_ex, datestring_ex,
-						  full_address_ex, street_address_ex, city_state_ex, email_ex,
-						  location_ex, description_ex, url_ex, city_ex, state_ex]
-
-		for i in range(len(names)):
-			curr = classifier(names[i],
-							  possible_values[i],
-							  regex[i],
-							  known_examples[i])
-			self.heuristic_classifiers.append(curr)
+		# Heuristic part
+		# one or more guesses
+		prediction2 = self.heuristic_classifier.classify(token)
+		
+		# Figure out the final guess through matching
+		if prediction1 in prediction2:
+			return prediction1
+		
+		return 'misc'
