356959c9393acf5469f592adb7aac69516954cda
kkravis000 <keithkravis2018@u.northwestern.edu>
2016-08-17 14:42:12 -0500

    Restructuring: the conclusion?

--- error_correction.py
+++
@@ -1,9 +0,0 @@
-#format checks
-#letter counts
-#dates certain way
-#two different correct formats, standardize to the same format, relates to harmonization
-
-class column_typer:
-	def __init__(self, error_list):
-	
-		
