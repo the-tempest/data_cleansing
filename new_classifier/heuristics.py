21b7047bb3d4aa52265eb5e0c3b2368e713ebb58
parwell <pawelklim1@gmail.com>
2016-08-18 22:44:09 -0500

    started massively revamping the way heuristics are written

--- new_classifier/heuristics.py
+++ new_classifier/heuristics.py
@@ -16,36 +16,9 @@
 # 4. return name of type being tested and point value (out of 100)
 
 # TODO massively simplify these
-# I think possible values should be taken totally out
 # example lists in features.py should also be much shorter
 # time complexity has been getting pretty bad lately
 
-NAME_LENGTH = 7
-NUM_NAME_SPACES = 1.5
-LOCATION_LENGTH = 9
-NUM_LOCATION_SPACES = 1
-
-FULL_NAME_POS      = 0
-FIRST_NAME_POS     = 1
-LAST_NAME_POS      = 2
-DATESTRING_POS     = 3
-FULL_ADDRESS_POS   = 4
-STREET_ADDRESS_POS = 5
-CITY_STATE_POS     = 6
-EMAIL_POS          = 7
-DESCRIPTION_POS    = 8
-URL_POS            = 9
-CITY_POS           = 10
-STATE_POS          = 11
-DATE_POS           = 12
-LONGITUDE_POS      = 13
-LATITUDE_POS       = 14
-LONGITUDE_POS      = 15
-ZIP_POS            = 16
-PHONE_POS          = 17
-IP_POS             = 18
-YEAR_POS           = 19
-ISBN_POS           = 20
 
 def full_name_heuristic(token, typer):
 	'''returns a  name heuristic value or negative infinity
@@ -55,6 +28,7 @@ def full_name_heuristic(token, typer):
 	value = generic_heuristic(token, typer, possibles, FULL_NAME_POS, split_token)
 	if value == 0:
 		return 'full name', 0
+
 	# misc part
 	misc_value = 0
 	lengths = [len(x) for x in split_token]
@@ -253,177 +227,101 @@ def city_heuristic(token, typer):
 	return 'city', value + misc_value
 
 def state_heuristic(token, typer):
-	'''returns a certainty value for token being a
-	state or zero if it definitely
-	isn't an state'''
-	split_token = token.split()
-	possibles = ['state', 'province', 'prefecture', 'territory']
-	# main part
-	value = generic_heuristic(token, typer, possibles, STATE_POS, split_token)
-	if value == 0:
-		return 'state', 0
-	# misc part
-	misc_value = 0
-	if len(split_token) in [1, 2]:
-		misc_value += 10
-
-	return 'state', value + misc_value
+	'''returns a certainty value for token being a state'''
+	possibles = ['state', 'province', 'territory']
+	my_classifier = typer.heuristic_classifiers['state']
+	value = generic_heuristic(token, typer, my_classifier, possibles)
+	return 'state', value
 
 def date_heuristic(token, typer):
-	'''returns a certainty value for token being a
-	date or zero if it definitely
-	isn't an state'''
-	split_token = token.split()
+	'''returns a certainty value for token being a date'''
 	possibles = ['date', 'day']
-	# main part
-	value = generic_heuristic(token, typer, possibles, DATE_POS, split_token)
-	if value == 0:
-		return 'date', 0
-	# misc part
-	# TODO improve
-	misc_value = 0
-	if len(token) == 8:
-		misc_value += 10
-
-	return 'date', value + misc_value
+	my_classifier = typer.heuristic_classifiers['date']
+	value = generic_heuristic(token, typer, my_classifier, possibles)
+	return 'date', value
 
 def longitude_heuristic(token, typer):
-	'''returns a certainty value for token being a
-	date or zero if it definitely
-	isn't an state'''
-	split_token = token.split()
-	possibles = ['longitude', 'position', 'location']
-	# main part
-	value = generic_heuristic(token, typer, possibles, LONGITUDE_POS, split_token)
-	if value == 0:
-		return 'longitude', 0
-	# misc part
-	# TODO improve
-	misc_value = 0
-	if token.count('.') == 1:
-		misc_value += 10
-
-	return 'longitude', value + misc_value
+	'''returns a certainty value for token being a longitude'''
+	possibles = ['longitude']
+	my_classifier = typer.heuristic_classifiers['longitude']
+	value = generic_heuristic(token, typer, my_classifier, possibles)
+	return 'longitude', value
 
 def latitude_heuristic(token, typer):
-	'''returns a certainty value for token being a
-	date or zero if it definitely
-	isn't an state'''
-	split_token = token.split()
-	possibles = ['latitude', 'position', 'location']
-	# main part
-	value = generic_heuristic(token, typer, possibles, LATITUDE_POS, split_token)
-	if value == 0:
-		return 'latitude', 0
-	# misc part
-	# TODO improve
-	misc_value = 0
-	if token.count('.') == 1:
-		misc_value += 10
-
-	return 'latitude', value + misc_value
+	'''returns a certainty value for token being a latitude'''
+	possibles = ['latitude']
+	my_classifier = typer.heuristic_classifiers['latitude']
+	value = generic_heuristic(token, typer, my_classifier, possibles)
+	return 'latitude', value
 
 def number_heuristic(token, typer):
-	'''returns a certainty value for token being a
-	date or zero if it definitely
-	isn't an state'''
-	split_token = token.split()
+	'''returns a certainty value for token being a number'''
 	possibles = ['number', 'count', 'quantity', 'amount']
-	# main part
-	value = generic_heuristic(token, typer, possibles, NUMBER_POS, split_token)
-	if value == 0:
-		return 'number', 0
-	# misc part
-	# TODO improve
-	misc_value = 0
-	if token.count('.') == 1:
-		misc_value += 10
-
-	return 'number', value + misc_value
+	my_classifier = typer.heuristic_classifiers['number']
+	value = generic_heuristic(token, typer, my_classifier, possibles)
+	return 'number', value
 
 def zip_heuristic(token, typer):
-	'''returns a certainty value for token being a
-	date or zero if it definitely
-	isn't an state'''
-	split_token = token.split()
-	possibles = ['zip', 'postal code']
-	# main part
-	value = generic_heuristic(token, typer, possibles, ZIP_POS, split_token)
-	if value == 0:
-		return 'zip', 0
-	# misc part
-	# TODO improve
-	misc_value = 0
-	if len(token) in [5, 9, 10]:
-		misc_value += 10
-
-	return 'zip', value + misc_value
+	'''returns a certainty value for token being an ip'''
+	possibles = ['zip', 'code']
+	my_classifier = typer.heuristic_classifiers['zip']
+	value = generic_heuristic(token, typer, my_classifier, possibles)
+	return 'zip', value
 
 def phone_heuristic(token, typer):
-	'''returns a certainty value for token being a
-	date or zero if it definitely
-	isn't an state'''
-	split_token = token.split()
-	possibles = ['phone', 'number', 'contact']
-	# main part
-	value = generic_heuristic(token, typer, possibles, PHONE_POS, split_token)
-	if value == 0:
-		return 'phone', 0
-	# misc part
-	# TODO improve
-	misc_value = 0
-	if token.count('(') and token.count(')'):
-		misc_value += 10
+	'''returns a certainty value for token being an phone'''
+	possibles = ['phone', 'number']
+	my_classifier = typer.heuristic_classifiers['phone_number']
+	value = generic_heuristic(token, typer, my_classifier, possibles)
+	return 'phone_number', value
 
-	return 'phone', value + misc_value
-
-def phone_heuristic(token, typer):
-	'''returns a certainty value for token being a
-	date or zero if it definitely
-	isn't an state'''
-	split_token = token.split()
+def ip_heuristic(token, typer):
+	'''returns a certainty value for token being an ip'''
 	possibles = ['ip', 'address']
-	# main part
-	value = generic_heuristic(token, typer, possibles, IP_POS, split_token)
-	if value == 0:
-		return 'ip', 0
-	# misc part
-	# TODO improve
-	misc_value = 0
-	if token.count('.') == 3:
-		misc_value += 10
+	my_classifier = typer.heuristic_classifiers['ip']
+	value = generic_heuristic(token, typer, my_classifier, possibles)
+	return 'ip', value
 
-	return 'ip', value + misc_value
+def year_heuristic(token, typer):
+	'''returns a certainty value for token being a year'''
+	possibles = ['year']
+	my_classifier = typer.heuristic_classifiers['year']
+	value = generic_heuristic(token, typer, my_classifier, possibles)
+	return 'year', value
 
-def generic_heuristic(token, typer, possibles, pos, split_token):
+def isbn_heuristic(token, typer):
+	'''returns a certainty value for token being an isbn'''
+	possibles = ['isbn', 'code']
+	my_classifier = typer.heuristic_classifiers['isbn']
+	value = generic_heuristic(token, typer, my_classifier, possibles)
+	return 'isbn', value
+
+def generic_heuristic(token, typer, my_classifier, possibles):
 	'''returns a certainty value for token being a
 	certain type'''
-	my_typer = typer.column_classifiers[pos]
 	
+	#check if it can't be the type
 	char_val_list = []
 	for char in token:
 		char_val_list.append(ord(char))
-
-	#check if it can't be the type
-	if not my_typer.can_be(char_val_list):
+	if not my_classifier.can_be(char_val_list):
 		return 0
 
 	value = 0
-
 	# check column name
 	for x in possibles:
-		if x in typer.curr_col_name.lower():
-			value += 20
+		if x in my_classifier.curr_col_name.lower():
+			value += 4
 			break
 
 	#account for the form of the token
-	if my_typer.has_form(token):
-		value += 30
+	if my_classifier.has_form(token):
+		value += 3
 
 	# check examples
-	for word in split_token:
+	for word in token.split():
 		if my_typer.is_a(word):
-			value += 40
+			value += 3
 			break
 
 	return value
@@ -433,6 +331,3 @@ heuristics = [full_name_heuristic, first_name_heuristic, last_name_heuristic,
 			 city_state_heuristic, email_heuristic,
 			 description_heuristic, url_heuristic, city_heuristic,
 			 state_heuristic]
-
-names = ['date', 'longitude', 'latitude', 'number', 'zip', 'phone_number',
-				'ip', 'year', 'isbn']
